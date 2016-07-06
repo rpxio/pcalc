676032cd68024ec8350ad1d3a58fbbd80fad485b
Randall Pace <randall@rpx.io>
2016-07-06 15:51:01 -0500

    init

---
+++ gulpfile.js
@@ -0,0 +1,52 @@
+var gulp = require('gulp');
+
+// require plugins
+var del = require('del');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+
+// clean task
+gulp.task('clean', function() {
+    return del([
+        './dist/**/*'
+    ]);
+});
+
+// copy task
+gulp.task('copy', function() {
+    return gulp
+        .src('./src/**/*.html')
+        .pipe(gulp.dest('dist'))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
+});
+
+// sass task
+gulp.task('sass', function() {
+    return gulp.src('./src/scss/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./dist/css'))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
+});
+
+// browsersync task
+gulp.task('browserSync', function() {
+    browserSync.init({
+        server: {
+            baseDir: 'dist'
+        },
+    })
+});
+  
+// watch task
+gulp.task('watch', ['browserSync', 'sass', 'copy'], function() {
+    gulp.watch('./src/scss/**/*.scss', ['sass']);
+    gulp.watch('./src/**/*.html', ['copy']);
+    gulp.watch('./src/js/**/*.js', browserSync.reload);
+});
+
+// default task
+gulp.task('default', ['clean']);
---
+++ readme.md
@@ -0,0 +1,10 @@
+# portfolio calculator
+
+small web project to calculate compounding interest and graph portfolio value from 1-30 years.
+
+### todo
+- make chart easier to read (learn to use d3.js?)
+- concatenate numbers like $100000 to $100k, $10500 to $10.5k, etc.
+- fix form layout
+
+:moneybag::moneybag::moneybag:
